6cb4e4af71c9ae4830d93b3d978505af6208f14e
Farhan <hey.farhan03@gmail.com>
2024-07-13 15:22:25 +0530

    login and signup routes updated

--- backend/Models/user.model.js
+++ backend/Models/user.model.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bycrpt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 const Schema = mongoose.Schema
 
@@ -29,4 +32,50 @@ const UserSchema = new Schema({
     },
 
 })
+
+
+UserSchema.statics.signup = async function (username, password, email, phone, admin) {
+
+    if (!username || !password || !email || !phone)
+        throw Error('Fill All the Fields')
+
+    const salt = await bycrpt.genSalt(10);
+    const hashPassword = await bycrpt.hash(password, salt);
+
+    const user = await this.create({ username, password: hashPassword, email, phone, admin })
+
+    return user
+
+}
+
+UserSchema.statics.login = async function (username, password) {
+
+    if (!username || !password)
+        throw Error('Fill All the Fields')
+
+    const user = await this.findOne({ username })
+
+    if (!user) {
+        throw Error('User Not Found')
+    }
+
+    const match = await bycrpt.compare(password, user.password)
+
+    if (!match)
+        throw Error('Incorrect Password')
+
+    return user
+}
+
+
+UserSchema.statics.createToken = async function (_id) {
+
+    const token = jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1h' })
+
+    return token
+}
+
+
+
 module.exports = mongoose.model('User', UserSchema)
+
